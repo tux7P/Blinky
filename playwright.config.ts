aecfaf1a32fc70e732e4c67fc821b7025a700775
tux7P <55027789+tux7P@users.noreply.github.com>
2025-04-19 18:50:32 +0300

    Update env path

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig, devices} from '@playwright/test';
 import dotenv from 'dotenv';
 
-dotenv.config({path: '.env.live'});
+dotenv.config({path: '.env'});
 
 export default defineConfig({
   testDir: './tests',
